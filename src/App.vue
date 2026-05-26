e11757ce63896c5b9d5a55a20dd2439d2a8adc2b
aaloui9603 <aaloui.9603@gmail.com>
2026-05-26 16:25:51 +0200

    feat: Landingpage - Kristalblau Design fertig

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const istGestartet = ref(false)
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="app-container">
+    <div v-if="!istGestartet" class="landing-card">
+      <div class="pearl-ring"></div>
+      <h1>📔 Vue-Diary</h1>
+      <p>Mein persönliches &amp; digitales Tagebuch <br>schreibe, markiere und speichere deine Gedanken</p>
+      <button @click="istGestartet = true">Tagebuch öffnen ✨</button>
+    </div>
+    <div v-else>
+      <h2>Willkommen in deinem Tagebuch 💙</h2>
+    </div>
+  </div>
+
 </template>
 
 <style scoped></style>
