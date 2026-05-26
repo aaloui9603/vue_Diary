8f6a45b35ace65e9ba0b87038ae868f07d90a1d9
aaloui9603 <aaloui.9603@gmail.com>
2026-05-26 17:05:31 +0200

    feat: Block 2 - Textfeld, v-model und computed fertig

--- src/components/DiaryEditor.vue
+++ src/components/DiaryEditor.vue
@@ -1,9 +1,33 @@
 <script setup>
+import { ref, computed } from 'vue'
+
+const titel = ref('')
+const notizInhalt = ref('')
+
+const zeichenAnzahl = computed(() => notizInhalt.value.length)
 </script>
 
 <template>
     <div class="diary-editor">
-        <h2>Mein Tagebuch 📔</h2>
+        <input 
+        v-model="titel"
+        placeholder="Titel deiner Notiz..."
+        class="titel-feld"
+        />
+
+        <textarea
+        v-model="notizInhalt"
+        placeholder="Schreibe deine Gedanken hier..."
+        class="grosses-textfeld"
+        ></textarea>
+
+        <div class="editor-footer">
+            <span class="zeichen-zaehler">{{  zeichenAnzahl  }} Zeichen</span>
+            <div class="toolbar-symbole">
+                <span class="symbol-btn" title="Speichern">💙</span>
+                <span class="symbol-btn" title="Neues Blatt">📜</span>
+            </div>
+        </div>
     </div>
 </template>
 
